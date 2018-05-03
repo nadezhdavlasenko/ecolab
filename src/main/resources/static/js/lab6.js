5472cf5d722abd8c0a23aca48fcda57ff03eef0f
Nadezhda Vlasenko <nadezhda277135@gmail.com>
2018-05-03 16:10:11 +0300

    map google

--- src/main/resources/static/js/lab6.js
+++ src/main/resources/static/js/lab6.js
@@ -21,7 +21,14 @@ $(document).ready(function() {
 
     var heatmapData = [];
     var heatmap;
-
+    var infowindow;
+    var contentString = '<h1 id="firstHeading" class="firstHeading">Запорiжжя</h1>'+
+                        '<table><tr><td>Речовина</td><td>Risk негайний</td><td>Risk хронiчний</td></tr>';
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(47,35),
+        map: map,
+        title: 'Uluru (Ayers Rock)'
+    });
 
 
     $.when($.ajax('http://localhost:8080/pollutant')).done(function () {
@@ -115,6 +122,20 @@ $(document).ready(function() {
                                 riskHot.setDataAtCell(currentRow, 2, response['mcl']);
                                 riskHot.setDataAtCell(currentRow, 4, response['prob']);
                                 riskHot.setDataAtCell(currentRow, 6, response['kreserve']);
+
+                                 var addString =
+                                     '<tr>'+
+                                     '<td>'+response['name']+'</td>'+ '<td>'+response['riskShort']+'</td>'+ '<td>'+response['riskLong']+'</td>'+
+                                     '</tr>';
+                                 contentString = contentString + addString;
+
+                                 infowindow = new google.maps.InfoWindow({
+                                    content: contentString + '</table>'
+                                });
+                                marker.addListener('click', function() {
+                                    infowindow.open(map, marker);
+                                });
+
                             }
                         });
                     }
@@ -132,39 +153,10 @@ function initMap() {
         center: {lat: 48.865427, lng: 34.196123},
     });
 
-    var contentString = '<div id="content">'+
-        '<div id="siteNotice">'+
-        '</div>'+
-        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-        '<div id="bodyContent">'+
-        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-        'sandstone rock formation in the southern part of the '+
-        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-        'south west of the nearest large town, Alice Springs; 450&#160;km '+
-        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-        'Aboriginal people of the area. It has many springs, waterholes, '+
-        'rock caves and ancient paintings. Uluru is listed as a World '+
-        'Heritage Site.</p>'+
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-        '(last visited June 22, 2009).</p>'+
-        '</div>'+
-        '</div>';
 
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
 
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(47,35),
-        map: map,
-        title: 'Uluru (Ayers Rock)'
-    });
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    });
+
+
 
 
 }
